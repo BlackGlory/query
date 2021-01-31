0372f66d5fa1b8d31ffc535c8c1c0d649df44319
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-31 02:36:55 +0000

    feat: add Node.js support

--- src/xpath.ts
+++ src/xpath.ts
@@ -1,10 +1,14 @@
 import { concat } from './shared/concat'
 
+const UNORDERED_NODE_ITERATOR_TYPE = 'XPathResult' in globalThis
+                                   ? XPathResult.UNORDERED_NODE_ITERATOR_TYPE
+                                   : 4
+
 export function xpath<T extends Element>(strings: TemplateStringsArray, ...values: string[]): (this: Document, parent: Node) => Iterable<T> {
   const expression = concat(strings, values).join('')
 
   return function* (this: Document, parent: Node) {
-    const iterator = this.evaluate(expression, parent, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null)
+    const iterator = this.evaluate(expression, parent, null, UNORDERED_NODE_ITERATOR_TYPE, null)
     let value
     while ((value = iterator.iterateNext()) !== null) {
       yield value as T
