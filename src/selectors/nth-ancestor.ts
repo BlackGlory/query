ea19574025149803c6a1038f2843d104b2961ac7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-04-24 06:55:32 +0000

    refactor: use extra-dom

--- src/selectors/nth-ancestor.ts
+++ src/selectors/nth-ancestor.ts
@@ -1,14 +1,5 @@
+import { parentNode } from 'extra-dom'
+
 export function nthAncestor<T extends Node>(nth: number): (node: Node) => T | null {
-  return (node: Node) => {
-    let currentNode = node
-    for (let i = 1; i <= nth; i++) {
-      const parentNode = currentNode.parentNode
-      if (parentNode) {
-        currentNode = parentNode
-      } else {
-        return null
-      }
-    }
-    return currentNode as T
-  }
+  return (node: Node) => parentNode(node, nth) as T | null
 }
