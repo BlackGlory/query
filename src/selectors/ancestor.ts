61703f7e8622cf164a46c78e6d6b9d066645db9d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-04-24 06:08:58 +0000

    refactor: use extra-dom

--- src/selectors/ancestor.ts
+++ src/selectors/ancestor.ts
@@ -1,20 +1,10 @@
+import { findInAncestorNodes } from 'extra-dom'
+
 export function ancestor<T extends Node & ParentNode>(
   predicate: (node: Node, nth: number) => unknown = () => true
 ): (node: Node) => T | null {
   return (node: Node) => {
-    let currentNode = node
     let i = 0
-    while (true) {
-      const parentNode = currentNode.parentNode
-      if (parentNode) {
-        if (predicate(parentNode, ++i)) {
-          return parentNode as T
-        } else {
-          currentNode = parentNode
-        }
-      } else {
-        return null
-      }
-    }
+    return findInAncestorNodes(node, node => predicate(node, ++i)) as T | null
   }
 }
